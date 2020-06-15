582d263415a5f8a4e67080124ad5a4297fa829ae
pikselan <faif@pikselan.com>
2020-06-15 11:19:18 +0700

    header gallery

--- src/pages/GalleryPage/index.js
+++ src/pages/GalleryPage/index.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
+import Fade from "react-reveal";
+
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+
+import GalleryBg from "../../assets/images/menu-bg.svg";
+
+import data from "../../json/GalleryPage.json";
 
 export default class index extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <header className="container-fluid v-max">
+          <Fade bottom>
+            <Nav pathname={this.props.location.pathname} />
+          </Fade>
+          <div
+            className="container text-center h-100"
+            style={{
+              background: `url(${GalleryBg}) no-repeat center`,
+              backgroundSize: "100% auto",
+            }}
+          >
+            <Fade bottom delay={300}>
+              <h1 className="h2 text-primary font-weight-bold d-sm-block d-lg-none">
+                {data.title}
+              </h1>
+              <h1 className="display-3 text-primary font-weight-bold d-none d-lg-block">
+                {data.title}
+              </h1>
+            </Fade>
+          </div>
+        </header>
+      </div>
+    );
   }
 }
